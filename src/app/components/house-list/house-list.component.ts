ee0a39bc61f36bb1699a14e0b9b649db899d0cca
yousraraed <yousraraed@hotmail.com>
2024-07-31 19:56:21 +0300

    correct filter

--- src/app/components/house-list/house-list.component.ts
+++ src/app/components/house-list/house-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Store, select } from '@ngrx/store';
-import { Observable, combineLatest } from 'rxjs';
-import { startWith, map } from 'rxjs/operators';
+import { Observable, combineLatest, BehaviorSubject } from 'rxjs';
+import { map, switchMap, startWith } from 'rxjs/operators';
 import {
   loadHouseModels,
   loadHouses,
@@ -24,17 +24,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./house-list.component.scss'],
 })
 export class HouseListComponent implements OnInit {
-  houses$?: Observable<HouseModel[]>;
-  houseModels$?: Observable<{ [key: string]: HouseModel }>;
-  loading$?: Observable<boolean>;
-  error$?: Observable<any>;
+  houses$!: Observable<HouseModel[]>;
+  houseModels$!: Observable<{ [key: string]: HouseModel }>;
+  loading$!: Observable<boolean>;
+  error$!: Observable<any>;
   filterForm: FormGroup;
-  housesGroupedByModel$?: Observable<{ [key: string]: HouseModelWithMedia }>;
-  houseModelsKeys$?: Observable<
+  housesGroupedByModel$!: Observable<{ [key: string]: HouseModelWithMedia }>;
+  houseModelsKeys$!: Observable<
     { title: string; media: MediaAttributes; house_type: string }[]
   >;
-  collapseStates$?: Observable<boolean[]>;
-  isAuthenticated$?: Observable<boolean>;
+  collapseStates$!: Observable<boolean[]>;
+  isAuthenticated$!: Observable<boolean>;
+  private filterFormValues$: BehaviorSubject<any>;
 
   constructor(
     private store: Store,
@@ -48,31 +49,35 @@ export class HouseListComponent implements OnInit {
       min_price: [''],
       max_price: [''],
     });
+
+    this.filterFormValues$ = new BehaviorSubject(this.filterForm.value);
   }
 
   ngOnInit(): void {
     this.reloadData();
-  }
-  reloadData() {
-    this.store.dispatch(loadHouseModels());
-    this.store.dispatch(loadHouses());
-    this.houses$ = this.store.pipe(select(selectAllHouses));
-    this.houseModels$ = this.store.pipe(select(selectAllHouseModels));
-    this.loading$ = this.store.pipe(select(selectHouseLoading));
-    this.error$ = this.store.pipe(select(selectHouseError));
-    this.isAuthenticated$ = this.store.pipe(select(selectIsAuthenticated));
-    const filterFormValues$ = this.filterForm.valueChanges.pipe(
-      startWith(this.filterForm.value)
-    );
+
+    const formValues$ = this.filterFormValues$
+      .asObservable()
+      .pipe(
+        switchMap(() =>
+          this.filterForm.valueChanges.pipe(startWith(this.filterForm.value))
+        )
+      );
 
     this.housesGroupedByModel$ = combineLatest([
       this.houses$,
       this.houseModels$,
-      filterFormValues$,
+      formValues$,
     ]).pipe(
       map(([houses, houseModels, filterValues]) => {
-        const filteredHouses = this.filterHouses(houses, filterValues);
-        return this.groupHousesByModel(filteredHouses, houseModels);
+        const filteredHouses = this.filterHouses(
+          houses as HouseModel[],
+          filterValues
+        );
+        return this.groupHousesByModel(
+          filteredHouses,
+          houseModels as { [key: string]: HouseModel }
+        );
       })
     );
 
@@ -90,6 +95,17 @@ export class HouseListComponent implements OnInit {
       map((houseModelsKeys) => new Array(houseModelsKeys.length).fill(false))
     );
   }
+
+  reloadData() {
+    this.store.dispatch(loadHouseModels());
+    this.store.dispatch(loadHouses());
+    this.houses$ = this.store.pipe(select(selectAllHouses));
+    this.houseModels$ = this.store.pipe(select(selectAllHouseModels));
+    this.loading$ = this.store.pipe(select(selectHouseLoading));
+    this.error$ = this.store.pipe(select(selectHouseError));
+    this.isAuthenticated$ = this.store.pipe(select(selectIsAuthenticated));
+  }
+
   filterHouses(houses: HouseModel[], filterValues: any): HouseModel[] {
     const { house_number, block_number, land_number, min_price, max_price } =
       filterValues;
@@ -128,6 +144,12 @@ export class HouseListComponent implements OnInit {
           };
         }
         housesGroupedByModel[model].houses.push(house);
+      }
+    });
+
+    Object.keys(housesGroupedByModel).forEach((model) => {
+      if (housesGroupedByModel[model].houses.length === 0) {
+        delete housesGroupedByModel[model];
       }
     });
 
